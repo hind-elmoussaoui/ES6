87efbdfe7317a9311931a1ad84a0432472960bdb
hind-elmoussaoui <hindelmoussaoui40@gmail.com>
2025-01-18 17:24:03 +0100

    Create century.js

---
+++ Numbers/century.js
@@ -0,0 +1,10 @@
+///////////////// Siécle à partir de l'année ///////////////////////
+function century(year) {
+    // Finish this :)
+    return Math.ceil(year / 100);
+}
+console.log(century(1705));
+console.log(century(1900));
+console.log(century(1601));
+console.log(century(2000));
+console.log(century(89));
