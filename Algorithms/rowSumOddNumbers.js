63be9ef469f850a046dd6faa3f4ef68913ff81ce
hind-elmoussaoui <hindelmoussaoui40@gmail.com>
2025-01-18 16:52:21 +0100

    Create rowSumOddNumbers.js

---
+++ Algorithms/rowSumOddNumbers.js
@@ -0,0 +1,8 @@
+///////////////// Somme de nombres impaire ///////////////////////
+function rowSumOddNumbers(n){
+    return Math.pow(n, 3)
+}
+console.log(rowSumOddNumbers(1));
+console.log(rowSumOddNumbers(2));
+console.log(rowSumOddNumbers(3));
+console.log(rowSumOddNumbers(4));
