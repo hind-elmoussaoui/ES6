6db1ceb7e206cb8dfa99fb6857f77a45666aa5bf
hind-elmoussaoui <hindelmoussaoui40@gmail.com>
2025-01-18 17:19:40 +0100

    Create numberToString.js

---
+++ DataType/numberToString.js
@@ -0,0 +1,7 @@
+///////////////// convertissez un nombre en chaîne ///////////////////////
+function numberToString(num) {
+    return num.toString();
+}
+  console.log(numberToString(123));  //--> "123"
+  console.log(numberToString(999));  //--> "999"
+  console.log(numberToString(-100)); //--> "-100"
