20f18eb8e2d228ff151f9c5c079aede662ce4ea2
hind-elmoussaoui <hindelmoussaoui40@gmail.com>
2025-01-18 17:55:20 +0100

    Create greet.js

---
+++ Basic Language Features/greet.js
@@ -0,0 +1,10 @@
+///////////////// Jenny's secret message ///////////////////////
+function greet(name){
+    if(name === "Johnny"){
+        return "Hello, my love!";
+    }
+    return "Hello, " + name + "!";
+}
+console.log(greet("Jim")); // "Hello, Jim!"
+console.log(greet("Jane")); // "Hello, Jane!"
+console.log(greet("Simon")); // "Hello, Simon!"
