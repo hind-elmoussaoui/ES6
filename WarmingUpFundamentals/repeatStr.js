5ffa9efd769294e9197a91de39bc589a7e5bb0e3
hind-elmoussaoui <hindelmoussaoui40@gmail.com>
2025-01-18 16:10:38 +0100

    Create repeatStr.js

---
+++ WarmingUpFundamentals/repeatStr.js
@@ -0,0 +1,6 @@
+///////////////// String repeat ///////////////////////
+function repeatStr(n, s){
+    return s.repeat(n);
+}
+console.log(repeatStr(6, "I"));       // "IIIIII"
+console.log(repeatStr(5, "Hello"));   // "HelloHelloHelloHelloHello"
